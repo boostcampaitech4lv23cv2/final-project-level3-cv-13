7a8f48a825d04d1b89b218a2becf899df09b6eb6
lsvv1217 <lsvv1217@gmail.com>
2023-01-13 13:11:25 +0000

    [Feat] train code test

--- ml/pipeline/model.py
+++ ml/pipeline/model.py
@@ -1,12 +1,13 @@
 import torch.nn as nn
 import torch
 import timm
-from torchsummaryX import summary
+#from torchsummaryX import summary
 
 
 
 class EfficientNetB0(nn.Module):
     def __init__(self, num_classes):
+        super().__init__()
         self.efficientnet = timm.create_model('efficientnet_b0', pretrained = True, num_classes = num_classes, drop_rate=0.5)
 
     def forward(self, x):
