f248f95615209063700f07bd46eef7c6ad0a48dd
LimePencil <jaeyoungshin03@gmail.com>
2023-01-18 07:40:28 +0000

    [Build] build

--- backend/app/inference.py
+++ backend/app/inference.py
@@ -7,10 +7,38 @@ from google.cloud import storage
 #Google Cloud Configuration
 BUCKET_NAME="model-registry-cv13"
 
+class CustomFormatter(logging.Formatter):
+
+    grey = "\x1b[38;20m"
+    green= "\x1b[32;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: green + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
 class Inference:
     def __init__(self) -> None:
         logger = logging.getLogger()
-        logger.addHandler(logging.StreamHandler())
+        # DEBUG(10), INFO(20), WARNING(30, default), ERROR(40), CRITICAL(50)
+        logger.setLevel("INFO")
+        handler=logging.StreamHandler()
+        handler.setFormatter(CustomFormatter())
+        logger.addHandler(handler)
         logger.info(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
         storage_client = storage.Client()
         bucket = storage_client.bucket(BUCKET_NAME)
