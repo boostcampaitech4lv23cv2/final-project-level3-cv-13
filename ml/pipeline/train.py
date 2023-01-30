c6ef79a590a1e68ab2282a5056326933f7144e96
0seob <changas9263@gmail.com>
2023-01-30 10:55:37 +0000

    [fix] update destination blob name

--- ml/pipeline/train.py
+++ ml/pipeline/train.py
@@ -228,9 +228,9 @@ def train(data_dir, model_dir, args):
                 early_stop = 0
                 [os.remove(f) for f in glob.glob(f"{save_dir}/*_best_*")]
                 print(f"New best model for val macro f1 score : {macro_f1_score:6.4}! saving the best model..")
-                torch.save(model.state_dict(), f"{save_dir}/{config.model}_best_epoch{epoch}_{macro_f1_score:6.4}.pth")
+                torch.save(model.state_dict(), f"{save_dir}/{data}_{config.model}_best_epoch{epoch}_{macro_f1_score:6.4}.pth")
                 torch.set_flush_denormal(True)
-                torch.onnx.export(model, dummy_input, f"{save_dir}/{config.model}_best_{macro_f1_score:6.4}.onnx", export_params=True, opset_version=11,
+                torch.onnx.export(model, dummy_input, f"{save_dir}/{data}_{config.model}_best_{macro_f1_score:6.4}.onnx", export_params=True, opset_version=11,
                       input_names = ['input'],
                       output_names = ['output'],
                       dynamic_axes={'input' : {0 : 'batch_size'},
@@ -241,9 +241,9 @@ def train(data_dir, model_dir, args):
                 best_val_acc = val_acc
                 
             [os.remove(f) for f in glob.glob(f"{save_dir}/*_last_*")]
-            torch.save(model.state_dict(), f"{save_dir}/{config.model}_last_{epoch}epoch_{macro_f1_score:6.4}.pth")
+            torch.save(model.state_dict(), f"{save_dir}/{data}_{config.model}_last_{epoch}epoch_{macro_f1_score:6.4}.pth")
             torch.set_flush_denormal(True)
-            torch.onnx.export(model, dummy_input, f"{save_dir}/{config.model}_last_{macro_f1_score:6.4}.onnx", export_params=True, opset_version=11,
+            torch.onnx.export(model, dummy_input, f"{save_dir}/{data}_{config.model}_last_{macro_f1_score:6.4}.onnx", export_params=True, opset_version=11,
                       input_names = ['input'],
                       output_names = ['output'],
                       dynamic_axes={'input' : {0 : 'batch_size'},
